dbfdc0ca2c77e7785379b1faf96574ef868c41fe
MaxPyvovarov <max.pivovar@gmail.com>
2022-10-17 20:15:08 +0300

    Popular component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 export default function Popular() {
 	const [popular, setPopular] = useState([]);
@@ -9,22 +12,88 @@ export default function Popular() {
 	}, []);
 
 	const getPopular = async () => {
-		const api = await fetch(
-			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-		);
+		if (localStorage.getItem('popular')) {
+			setPopular(JSON.parse(localStorage.getItem('popular')));
+		} else {
+			const api = await fetch(
+				`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+			);
 
-		const data = await api.json();
-		setPopular(data.recipes);
-		console.log(popular);
+			const data = await api.json();
+			localStorage.setItem('popular', JSON.stringify(data.recipes));
+			setPopular(data.recipes);
+		}
 	};
 
 	return (
 		<div>
-			{popular.map(recipe => (
-				<div key={recipe.id}>
-					<p>{recipe.title}</p>
-				</div>
-			))}
+			<Wrapper>
+				<h3>Popular picks</h3>
+				<Splide
+					options={{
+						perPage: 4,
+						arrows: false,
+						pagination: false,
+						drag: 'free',
+						gap: '5rem',
+					}}
+				>
+					{popular.map(recipe => (
+						<SplideSlide key={recipe.id}>
+							<Card>
+								<p>{recipe.title}</p>
+								<img src={recipe.image} alt={recipe.title} />
+								<Gradient />
+							</Card>
+						</SplideSlide>
+					))}
+				</Splide>
+			</Wrapper>
 		</div>
 	);
 }
+
+const Wrapper = styled.div`
+	margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+	position: relative;
+	min-height: 13rem;
+	border-radius: 2rem;
+	overflow: hidden;
+
+	img {
+		border-radius: 2rem;
+		position: absolute;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	p {
+		position: absolute;
+		z-index: 10;
+		left: 50%;
+		bottom: 0;
+		transform: translate(-50%, 0%);
+		color: #fff;
+		width: 100%;
+		text-align: center;
+		font-weight: 600;
+		font-size: 1rem;
+		height: 40%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+`;
+
+const Gradient = styled.div`
+	z-index: 3;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
